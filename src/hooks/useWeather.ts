1a7341b677aeff9d76fbfc278ced6cc4b568c73d
irenemonzon <irene.monzonm@gmail.com>
2024-05-04 11:28:57 +1000

    use api climate and install axios

---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,48 @@
+import axios from "axios"
+import { SearchType,Weather } from "../types"
+
+
+function isWeatherResponde(weather:unknown):weather is Weather{
+    return(
+        Boolean(weather) &&
+        typeof weather ==='object' &&
+        typeof(weather as Weather).name==='string' &&
+        typeof(weather as Weather).main.temp ==='number' &&
+        typeof(weather as Weather).main.temp_max==='number' &&
+        typeof(weather as Weather).main.temp_min==='number'
+    )
+
+}
+
+export default function useWeather(){
+
+    const fetchWeather=async(search:SearchType)=>{
+        const appId=import.meta.env.VITE_API_KEY
+
+        try{
+
+            const geoUrl=`http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+
+            const {data}=await axios(geoUrl)
+            const lat= data[0].lat
+            const lon=data[0].lon
+
+            const weatherUrl=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+            const {data:weatherResult}= await axios(weatherUrl)
+           const result=isWeatherResponde(weatherResult)
+
+          
+        }catch(error){
+            console.log(error)
+        }
+
+    }
+
+
+
+    return{
+        fetchWeather
+
+    }
+}
